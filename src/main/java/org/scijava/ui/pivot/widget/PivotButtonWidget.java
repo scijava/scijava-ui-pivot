b5ff6b5a3a3eab3c4898e9371ea096fbca626801
Curtis Rueden <ctrueden@wisc.edu>
2015-02-11 07:07:08 -0600

    Happy belated New Year 2015

--- src/main/java/org/scijava/ui/pivot/widget/PivotButtonWidget.java
+++ src/main/java/org/scijava/ui/pivot/widget/PivotButtonWidget.java
@@ -2,7 +2,7 @@
  * #%L
  * SciJava UI components for Apache Pivot.
  * %%
- * Copyright (C) 2011 - 2014 Board of Regents of the University of
+ * Copyright (C) 2011 - 2015 Board of Regents of the University of
  * Wisconsin-Madison.
  * %%
  * Redistribution and use in source and binary forms, with or without
